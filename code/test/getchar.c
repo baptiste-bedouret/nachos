2bbbf4b89da3ac31c6481e97da8bfb16701694ce
Abrahim-BAMATRAF <ebrahem.f.b@gmail.com>
2021-10-12 11:36:44 +0200

    finished testing getchar

--- code/test/getchar.c
+++ code/test/getchar.c
@@ -1,3 +1,10 @@
+/**
+ * @brief reads one character given by the user.
+ * @details In the function main the method GetChar reads one character then we use the method PutChar displays that character,
+ * if the method GetChar was given more then one character it will only reads the first one
+ * @param.
+ * @return char (the character).
+*/
 #include "syscall.h"
 
 int 
